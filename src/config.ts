70d454b2a4ecbec99a6b2b296b09f8e03f91d413
YK <osakana.yano49@gmail.com>
2020-12-22 18:02:49 +0900

    add config class

---
+++ src/config.ts
@@ -0,0 +1,41 @@
+import * as vscode from 'vscode';
+
+export class Config {
+    private getConfiguration = vscode.workspace.getConfiguration;
+    private section: string = 'sonicpieditor';
+    private rubySection: string = 'ruby.interpreter';
+
+    // sonic-pi's config
+    public launchSonicPiServerAutomatically(): string {
+        return this.getConfiguration(this.section).launchSonicPiServerAutomatically;
+    }
+    public runFileWhenRunSelectedIsEmpty(): string {
+        return this.getConfiguration(this.section).runFileWhenRunSelectedIsEmpty;
+    }
+    public sonicPiRootDirectory(): string {
+        return this.getConfiguration(this.section).sonicPiRootDirectory;
+    }
+    public launchSonicPiServerCustomExtension(): string {
+        return this.getConfiguration(this.section).launchSonicPiServerCustomExtension;
+    }
+    public invertStereo(): boolean {
+        return this.getConfiguration(this.section).invertStereo;
+    }
+    public forceMono(): boolean {
+        return this.getConfiguration(this.section).forceMono;
+    }
+    public logClearOnRun(): boolean {
+        return this.getConfiguration(this.section).logClearOnRun;
+    }
+    public safeMode(): boolean {
+        return this.getConfiguration(this.section).safeMode;
+    }
+    public updateRunFileWhenRunSelectedIsEmpty(value: string) {
+        this.getConfiguration(this.section).update('runFileWhenRunSelectedIsEmpty', value, true);
+    }
+
+    // ruby interpreter's config
+    public commandPath(): string {
+        return this.getConfiguration(this.rubySection).commandPath;
+    }
+}
